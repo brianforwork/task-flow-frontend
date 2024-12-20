100ae3c2d1eb73d8adeddf4eae2ec76db9f5f6ed
Mac <mac@eduroam-226-106-0.laptops.usf.edu>
2024-12-20 17:11:53 -0500

    Add Light/Dark Mode

--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,26 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { pink } from '@mui/material/colors'
 
 
 // Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    mode: 'dark',     
-    primary: {
-      main: red[500] 
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: pink[600]
+        }
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
+    dark: {
+      palette: {
+        primary: {
+          main: pink[400]
+        }
+      }
+    }
   }
+  // ...other properties
 })
 
 export default theme
