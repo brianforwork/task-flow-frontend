e2e43124282ef4d5202f2eeadcc655012985acba
Mac <mac@eduroam-226-125-43.laptops.usf.edu>
2024-12-22 19:23:40 -0500

    Refactor Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,91 +1,11 @@
-import { useColorScheme } from '@mui/material/styles'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import Box from '@mui/material/Box'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeIcon from '@mui/icons-material/DarkMode'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import Container from '@mui/material/Container'
-
-function ModeSelect() {
-
-  const { mode, setMode } = useColorScheme()
-
-  const handleChange = (event) => {
-    setMode(event.target.value)
-  }
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">{mode}</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value='light'>
-          <Box sx={{ display:'flex', alignItems:'center', gap: 1 }} >
-            <LightModeIcon fontSize='small'/>
-            Light
-          </Box>
-        </MenuItem>
-        <MenuItem value='dark'>
-          <Box sx={{ display:'flex', alignItems:'center', gap: 1 }} >
-            <DarkModeIcon fontSize='small'/>
-            Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value='system'>
-          <Box sx={{ display:'flex', alignItems:'center', gap: 1 }} >
-            <SettingsBrightnessIcon fontSize='small'/>
-            System
-          </Box>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  )
-}
+import Board from './pages/Boards/_id'
 
 function App() {
-
-
   return (
-    <Container disableGutters maxWidth='false' sx={{ height: '100vh' }}>
-      <Box sx={{
-        width: '100%',
-        backgroundColor: 'primary.light',
-        height: (theme) => theme.trello.appBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        <ModeSelect />
-        App Bar
-      </Box>
-
-      <Box sx={{
-        width: '100%',
-        backgroundColor: 'primary.dark',
-        height: (theme) => theme.trello.boardBarHeight,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board Bar
-      </Box>
-
-      <Box sx={{
-        width: '100%',
-        backgroundColor: 'white',
-        height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-        display: 'flex',
-        alignItems: 'center'
-      }}>
-        Board Content
-      </Box>
-    </Container>
+    <>
+      {/* REACT ROUTER DOM */}
+      <Board />
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -13,3 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </CssVarsProvider>
   </React.StrictMode>
 )
+//   <React.StrictMode>
+//<CssVarsProvider theme={theme}>
+//<CssBaseline />
+//<App />
+//</CssVarsProvider>
+//</React.StrictMode>
