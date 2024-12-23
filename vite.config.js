928fc9628e1993876f5a774d38b408542abb09f5
Mac <mac@eduroam-226-125-43.laptops.usf.edu>
2024-12-23 16:39:52 -0500

    App Bar 1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr' // Used for Icon in MUI with svg file
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr()
+  ],
   // base: './'
+  // For absolute import
   resolve: {
     alias: [
       { find: '~', replacement: '/src' }
