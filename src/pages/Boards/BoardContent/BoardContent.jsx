f11ad35d79f78997e5815d430364e808934b3d3e
Mac <mac@Brians-Macbook.local>
2024-12-31 00:46:30 -0500

    Update Drag/Drop for Columns with dndkit

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,20 +1,47 @@
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sort'
+import { DndContext, useSensor, useSensors, MouseSensor, TouchSensor } from '@dnd-kit/core'
+import { useEffect, useState } from 'react'
+import { arrayMove } from '@dnd-kit/sortable'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
-  return (
-    <Box sx={{
-      width: '100%',
-      p: '10px 0',
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      height: (theme) => theme.trello.boardContentHeight
-    }}>
-      {/* Column */}
-      <ListColumns columns={orderedColumns} />
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+  const sensors = useSensors(mouseSensor, touchSensor)
 
-    </Box>
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event
+    if (!over) return
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // Not Update the database yet
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
+  return (
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box sx={{
+        width: '100%',
+        p: '10px 0',
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
+        height: (theme) => theme.trello.boardContentHeight
+      }}>
+        {/* Column */}
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
+
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -2,41 +2,45 @@ import Box from '@mui/material/Box'
 import Column from './Column/Column'
 import Button from '@mui/material/Button'
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd'
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListColumns({ columns }) {
 
   return (
-    <Box sx={{
-      bgcolor: 'inherit',
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      overflowX: 'auto',
-      overflowY: 'hidden',
-      '&::-webkit-scrollbar-track': { m:2 }
-    }}>
-      {/*Box Column 1*/}
-      {columns?.map(column => <Column column={column} key={column._id} />)}
+    <SortableContext items={columns?.map(c => c._id)} strategy={horizontalListSortingStrategy}>
       <Box sx={{
-        minWidth: '200px',
-        maxWidth: '200px',
-        height: 'fit-content',
-        mx: 2,
-        bgcolor: '#ffffff3d',
-        borderRadius: '6px'
+        bgcolor: 'inherit',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        overflowX: 'auto',
+        overflowY: 'hidden',
+        '&::-webkit-scrollbar-track': { m:2 }
       }}>
-        <Button startIcon={<PlaylistAddIcon />} sx={{
-          color: 'white',
-          pl: 2.5,
-          py: 1,
-          justifyContent: 'flex-start',
-          width: '100%'
-        }}
-        >
+        {/*Box Column 1*/}
+        {columns?.map(column => <Column column={column} key={column._id} />)}
+        <Box sx={{
+          minWidth: '200px',
+          maxWidth: '200px',
+          height: 'fit-content',
+          mx: 2,
+          bgcolor: '#ffffff3d',
+          borderRadius: '6px'
+        }}>
+          <Button startIcon={<PlaylistAddIcon />} sx={{
+            color: 'white',
+            pl: 2.5,
+            py: 1,
+            justifyContent: 'flex-start',
+            width: '100%'
+          }}
+          >
           Add New Column
-        </Button>
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
+
   )
 }
 
